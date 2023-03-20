04dbd4f3f9d038779be5aacade8e8292e7d204f7
GersonMembrillo <gersonmembrillo@hotmail.com>
2023-03-20 16:43:27 +0100

    remove numbers with css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/*
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+ */
+
 const levelForm = document.getElementById('levelForm');
 levelForm.addEventListener('submit', play);
 
@@ -8,7 +19,7 @@ function drawSquare(content, sideNumSquares) {
 	square.classList.add('square');
 	square.style.width = `calc(100% / ${sideNumSquares})`;
 	square.style.height = square.style.width;
-	square.innerHTML = content;
+	square.innerHTML = `<span>${content}</span>`;
 	return square;
 }
 
@@ -34,7 +45,7 @@ function showAllBombs(bombs) {
 	const squares = document.querySelectorAll('.square');
 	for (let square of squares) {
 		if (bombs.includes(parseInt(square.innerText))) {
-			square.classList.add('unsafe');
+			square.classList.add('boom');
 		}
 	}
 }
@@ -90,6 +101,7 @@ function play(e) {
 					this.classList.add('boom');
 					message = `Hai perso ! Il tuo punteggio è : ${score}`;
 					gameOver = true;
+					showAllBombs(bombs);
 				} else {
 					this.classList.add('safe');
 					score++;
